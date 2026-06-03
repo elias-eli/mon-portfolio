1a53a0b1084e5a55ef78d2923c1550971b9f5d64
elias-eli <92509270+elias-eli@users.noreply.github.com>
2026-06-03 12:13:59 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach((tag, index) => {
+        tag.style.opacity = '0';
+        tag.style.transform = 'translateY(10px)';
+        setTimeout(() => {
+            tag.style.transition = 'all 0.5s ease';
+            tag.style.opacity = '1';
+            tag.style.transform = 'translateY(0)';
+        }, 300 + (index * 100));
+    });
+});
